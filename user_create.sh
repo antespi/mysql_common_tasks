bee27506f17a95e85e9cbd39830b27bae52ce6ba
Antonio Espinosa <aespinosa@teachnova.com>
2013-09-10 13:17:45 +0200

    Check if user already exists

--- user_create.sh
+++ user_create.sh
@@ -2,6 +2,10 @@
 
 if [ ! $# -eq 2 ]; then echo "Usage: $0 <username> <password>"; exit 1; fi
 
+exists=`echo "SELECT User FROM mysql.user WHERE User = '$1';" | mysql`
+
+if [ -n "$exists" ]; then echo "User $1 already exists"; exit 2; fi
+
 echo "CREATE USER '$1'@'localhost' IDENTIFIED BY  '$2';" | mysql || exit $?
 echo "GRANT USAGE ON * . * TO  '$1'@'localhost' IDENTIFIED BY  '$2' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;" | mysql
 
