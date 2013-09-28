6cee803c9dc9051d0d684d93efccfd5b2417acee
root <root@teachnova.com>
2013-09-28 13:29:10 +0200

    Rename root_passwd and make possible to change password for any user

--- user_passwd.sh
+++ user_passwd.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ ! $# -eq 0 ]; then echo "Usage: $0"; exit 1; fi
+if [ ! $# -eq 1 ]; then echo "Usage: $0 <user>"; exit 1; fi
 
+USER="$1"
 PASSWORD_NEW=
 RE_PASSWORD_NEW=
 PASSWORD_CURRENT=
 
-read -s -p "Enter current password: " PASSWORD_CURRENT; echo
+read -s -p "Enter root password: " PASSWORD_CURRENT; echo
 
 if ! echo "SHOW DATABASES;" | mysql -u root --password="$PASSWORD_CURRENT" > /dev/null 2>&1 ; then
    echo "Bad password, try again"; exit 2;
@@ -19,4 +20,4 @@ if [ "$PASSWORD_NEW" != "$RE_PASSWORD_NEW" ]; then
    echo "New passwords are not equal, try again"; exit 3;
 fi
 
-echo "USE mysql; UPDATE user SET Password=PASSWORD('$PASSWORD_NEW') where User='root'; FLUSH PRIVILEGES;" | mysql -u root --password="$PASSWORD_CURRENT"
+echo "USE mysql; UPDATE user SET Password=PASSWORD('$PASSWORD_NEW') where User='$USER'; FLUSH PRIVILEGES;" | mysql -u root --password="$PASSWORD_CURRENT"
